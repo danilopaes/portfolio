787a69f612636f780eb1af375736d3bd7e8d1a83
danilopaes <danilopaesribeiro@gmail.com>
2025-01-02 16:36:40 -0300

    style: ajusta formatação do CSS e HTML

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-const menuHamburguer = document.querySelector('.menu-hamburguer')
-  menuHamburguer.addEventListener('click', () => {
-      toggleMenu();
-  });
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+menuHamburguer.addEventListener('click', () => {
+  toggleMenu();
+});
 
-  function toggleMenu(){
-    const menu = document.querySelector('.nav-responsive');
-    menuHamburguer.classList.toggle('change');
-
-    if (menuHambuguer.classList.contains('change')){
-      nav.style.display = 'block';
-    }else{
-      nav.style.display = 'none';
-    }
+function toggleMenu() {
+  const nav = document.querySelector('.nav-responsive');
+  menuHamburguer.classList.toggle('change'); // Corrigido o nome da variável
+  if (menuHamburguer.classList.contains('change')) { // Corrigido o nome da variável
+    nav.style.display = 'block';
+  } else {
+    nav.style.display = 'none';
   }
+}
